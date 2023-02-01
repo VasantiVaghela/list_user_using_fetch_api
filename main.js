7c64203043214ff30320187e8f0258313323323b
kevit-vasanti-vaghela <vasanti.vaghela@kevit.io>
2023-02-01 13:58:29 +0530

    in progress

--- main.js
+++ main.js
@@ -10,24 +10,11 @@ let submit = document.querySelector('#submit')
 let form = document.querySelector('.user-details')
 let addUserBtn = document.querySelector('.add-user')
 let addNewUserBtn = document.querySelector('.add-new-user')
-let tableHeader = document.querySelector('.header')
+
 let result;
 
 // Get User List
 getUsers()
-
-
-
-function getUsers() {
-    axios
-        .get('https://reqres.in/api/users?page=2')
-        .then(({data:{data}}) => {
-            result = data
-            console.log("data",data)  // User Data List
-            showOutput(data)
-        })
-        .catch(err => console.log(err));
-}   
 
 //ADD NEW USER
 addUserBtn.addEventListener('click', function() {
@@ -40,26 +27,41 @@ addUserBtn.addEventListener('click', function() {
     u_img.setAttribute('placeholder','Picture')
 });      
     // document.querySelector('.form-div').classList.add('visible')
-    addNewUserBtn.addEventListener('click', function(e) {
-        e.preventDefault()
-        addUser()                       
+    addNewUserBtn.addEventListener('click', function() {
+        addUser()  
+                           
 })
+
+function getUsers() {
+    axios
+        .get('https://reqres.in/api/users')
+        .then(({data:{data}}) => {
+            result = data
+            console.log("data",data)  // User Data List
+            console.log('RESULT FROM GETUSER :',result)
+            showOutput(data)
+
+        })
+        .catch(err => console.log(err));
+}   
+
+
 
 // --------------------------------------------------------------------------
 //Delete the User
 function deleteUser(id) {
-    console.log('RESULT FROM DELETE',result )
+    console.log('RESULT BEFORE FROM DELETE',result )
     axios
       .delete('https://reqres.in/api/users/'+id)
       .then(res => {
-        console.log('deletedUser',res)
+        console.log('deletedUser',res.data)
         let newResult = result.filter(user => {
             if(id !== user.id){
                 return user
             }
          })
         result = [...newResult]
-        console.log(result)
+        console.log('RESULT FROM DELETE',result)
         showOutput(result)
         
     })
@@ -67,28 +69,38 @@ function deleteUser(id) {
 }
   
 //Update the User
-function updateUser(id) {
+function updateUser(idd) {
 
     axios
-    .put('https://reqres.in/api/users/'+id,{
+    .put('https://reqres.in/api/users/'+idd,{
         "first_name": fname.value,
         "last_name": lname.value,
-        "avatar": u_img.value
+        "avatar": u_img.value,
+       
        }
     )
     .then(res => {
         console.log('updateUser',res.data)
-        // console.log(result.indexOf(id))
-        result.filter((user) => {
-            if(id === user.id){
+        console.log('RESULT BEFORE UPDATE',result)
+            let index = result.findIndex((user) => {
+            
+            return user.id == idd
+            // if(id === user.id){
                 
-                user.first_name = res.data.first_name
-                user.last_name = res.data.last_name
-                user.avatar = res.data.avatar
-                return user
-            }
+            //     user.first_name = res.data.first_name
+            //     user.last_name = res.data.last_name
+            //     user.avatar = res.data.avatar
+            //     return user
+            // }
          })
+         if( index !== -1) {
+            result[index].first_name = res.data.first_name
+            result[index].last_name = res.data.last_name
+            result[index].avatar = res.data.avatar
+         }
         
+        
+        console.log('index ::',index)
         console.log('result ::',result)
         // let newResult = result
         showOutput(result)
@@ -100,17 +112,20 @@ function updateUser(id) {
 
 //Add New User
 function addUser() {
+    console.log('RESULT BEFORE ADD REQUEST',result)
+    let email =  `someone${Math.floor(Math.random() * 10000000000)}@example.com`
     axios
     .post('https://reqres.in/api/users',{
         "first_name": fname.value,
         "last_name": lname.value,
-        "avatar" : u_img.value
+        "avatar" : u_img.value,
+        "email" : email
     })
     .then(res => {
         console.log('addUser',res)
         console.log('addUser data',res.data)
         result.unshift(res.data)
-        console.log(result)
+        console.log('RESULT OF ADD REQUEST',result)
         showOutput(result)
        
     })
@@ -121,7 +136,7 @@ function addUser() {
 //Function to display data in DOM
 function showOutput(data) {
      // Display Data in DOM
-     let output = `<tr class="header" >
+     let output = `<tr class="header">
                         <th>Avatar</th>
                         <th>First Name</th>
                         <th>Last Name</th>
@@ -136,72 +151,43 @@ function showOutput(data) {
                 </tr>`;
 
         userTable.innerHTML = output;
-        //Show and Hide Form
-        
-        // tableHeader.addEventListener('click', function() {
-        //     document.querySelector('.form-div').classList.toggle('visible')
-        // });
+       
 
         
     });
-    // Fetch User Rows
-    // console.log('User Rows :',userRows)  // fetch user rows
-    // let userRowArray = Array.from(userRows)
-    // console.log(userRowArray)
-    // userRowArray.forEach(row => {
-    //     row.addEventListener('click',function() {
-    //         let cellsArray = Array.from(row.cells)
-    //         // console.log(cellsArray[0].firstChild)
-    //         u_img.value = cellsArray[0].firstChild['src']
-    //         fname.value = cellsArray[1].firstChild.data
-    //         lname.value = cellsArray[2].firstChild.data
-
-    //         submit.addEventListener('click', function() {
-
-    //             updateUser()
-    //         })
-    //     })
-    // })
-
+//      //Show and Hide Form
+//      let tableHeader = document.querySelector('.header')    
+//      tableHeader.addEventListener('click', function() {
+//         document.querySelector('.form-div').classList.toggle('visible')
+//     });
 }
 
-// function showSingleUser(res){
-//     let singleoutput = `<tr class="header">
-//                                 <th>Avatar</th>
-//                                 <th>First Name</th>
-//                                 <th>Last Name</th>
-//                                 <th></th>   
-//                             </tr>
-//                         <tr class="user">
-//                             <td class="avatar"><img class="avatar-img" src="${res.data.picture}" alt="${res.data.firstName}"></td>
-//                             <td>${res.data.firstName}</td>
-//                             <td>${res.data.lastName}</td>
-//                             <td><button class="delete-user">X</button></td>
-//                         </tr>   
-//                     `;
-//             userTable.innerHTML = singleoutput;
-// }
 
 function showTableDataInForm() {
     console.log('User Rows :',userRows)  // fetch user rows
     let userRowArray = Array.from(userRows)
     // console.log(userRowArray)
 
-    userRowArray.forEach(row => {
+    userRowArray.map(row => {
+        
         row.addEventListener('click',function() {
+            ID = row.getAttribute('id')
+            console.log('ID',ID)
             let cellsArray = Array.from(row.cells)
             // console.log(cellsArray[0].firstChild)
             u_img.value = cellsArray[0].firstChild['src']
             fname.value = cellsArray[1].firstChild.data
             lname.value = cellsArray[2].firstChild.data
+
+            
             
         })
+        form.addEventListener('submit', function(e) {
+            e.preventDefault()
+            updateUser(ID)
+            ID = undefined
+        })
+       
     })
 }
-    // form.addEventListener('submit',function(e) {
-    //     e.preventDefault()
-
-    //     updateUser(id)
-    // })
     
-// }
